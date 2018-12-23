30b659c9e984f3d7bf0209dc0ee1bb303c5c2d36
karolszymonczyk <karolszymonczyk@gmail.com>
2018-12-23 20:21:11 +0100

    Login panel added.

--- src/main/java/controllers/LoginPaneController.java
+++ src/main/java/controllers/LoginPaneController.java
@@ -1,11 +1,22 @@
 package controllers;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoginPaneController {
 
   private MainController controller;
+
+  private String login, password;
+
+  @FXML
+  private TextField tLogin, tPassword;
+
+  @FXML
+  private Button bLogin;
 
   @FXML
   public void initialize() {
@@ -15,5 +26,13 @@ public class LoginPaneController {
 
   public void setController(MainController controller) {
     this.controller = controller;
+  }
+
+  public void bLoginClick(ActionEvent event) {
+    login = tLogin.getText();
+    password = tPassword.getText();
+
+    System.out.println(login);
+    System.out.println(password);
   }
 }
